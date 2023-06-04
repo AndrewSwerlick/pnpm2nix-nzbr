3115b9c6464ac034937b4b1064b180edb891eaa9
Andrew Swerlick <andrew.swerlick@hey.com>
2023-06-03 22:58:20 -0400

    Add tracing

--- lockfile.nix
+++ lockfile.nix
@@ -7,7 +7,7 @@
 
 with lib;
 let
-  getName = fullname: head (builtins.split "@" (last (builtins.split "/" fullname)));
+  getName = fullname: head (builtins.split "@" (last (builtins.split "/" (builtins.trace fullname))));
   fixVersion = ver: head (builtins.split "_" ver);
   getVersion = fullname: fixVersion (last (builtins.split "@" (last (tail (builtins.split "/" fullname)))));
   getScope = fullname: head (builtins.split "/" fullname);
@@ -26,13 +26,10 @@ rec {
             name = getName name;
             version = getVersion n;
           in
-            {
-              (builtins.trace n);
-              fetchurl {
-                url = "${registry}/${path}/-/${name}-${version}.tgz";
-                sha512 = v.resolution.integrity;
-              };
-            }
+          fetchurl {
+            url = "${registry}/${path}/-/${name}-${version}.tgz";
+            sha512 = v.resolution.integrity;
+          }
         )
         (parseLockfile lockfile).packages
     );
